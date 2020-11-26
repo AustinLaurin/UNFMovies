560d3c839be52e5883110b8518986b83a3a31837
chloepolder <68082663+chloepolder@users.noreply.github.com>
2020-11-25 23:57:08 -0500

    Update UNFMovieStore.java

--- UNFMovieStore.java
+++ UNFMovieStore.java
@@ -18,10 +18,11 @@ public class UNFMovieStore {
                     String fname = input.next();
                     System.out.print("Enter your password: ");
                     String password = input.next();
-                    administrator a = new administrator();
-                    a.administrator(lname, fname, password);
+                    administrator a = new administrator(lname, fname, password);
                     
-                    System.out.print("Hello "+fname+", How would you like to proceed? Please enter 'locate title', 'update inventory', 'manage customer balance', or 'generate reports': ");
+                    int i = 0;
+                    while(i < 1){
+                    System.out.print(fname+", please select an action. Enter 'locate title', 'update inventory', 'manage customer balance', or 'generate reports': ");
                     input = new Scanner (System.in);
                     switch (input.nextLine()){
                         case "locate title":{
@@ -29,6 +30,8 @@ public class UNFMovieStore {
                             System.out.print("Enter the title you want to locate: ");
                             String title = input.nextLine();
                             a.locateTitle(title);
+                            i = 0;
+                            break;
                         }
                         case "update inventory":{
                         //call deleteMovieFromDatabase then call decreaseInventory?
@@ -52,9 +55,11 @@ public class UNFMovieStore {
                                     switch (input.next()){
                                         case "yes":{
                                             digital = 1;
+                                            break;
                                         }
                                         case "no":{
                                             digital = 0;
+                                            break;
                                         }
                                     }
                                     System.out.print("Is the movie a new or old release?(enter 'new' or 'old') ");
@@ -69,12 +74,14 @@ public class UNFMovieStore {
                                             newRate = input.nextDouble();
                                             System.out.print("Enter the rental period for the movie in days: ");
                                             newPeriod = input.nextInt();
+                                            break;
                                         }
                                         case "old":{
                                             System.out.print("Enter the rental rate for the movie: ");
                                             oldRate = input.nextDouble();
                                             System.out.print("Enter the rental period for the movie in days: ");
                                             oldPeriod = input.nextInt();
+                                            break;
                                         }
                                     }
                                     System.out.print("What would you like the late fee rate to be? ");
@@ -89,25 +96,33 @@ public class UNFMovieStore {
                                     String directorFirst = input.next();
                                     System.out.print("Is this movie is a sequel?(enter 'yes' or 'no') ");
                                     String sequel = null;
+                                    Integer sequelTo = null;
                                     input = new Scanner (System.in);
                                     switch (input.next()){
                                         case "yes":{
                                             System.out.print("Enter the ID of the prequel to this movie: ");
                                             sequel = input.next();
+                                            sequelTo = Integer.valueOf(sequel);
+                                            break;
                                         }
                                         case "no":{
-                                            sequel = null;
-                                        }  
+                                            sequelTo = null;
+                                            break;
+                                       }  
                                     }
-                                    int sequelTo = Integer.parseInt(sequel);
+                                    //int sequelTo = Integer.valueOf(sequel);
                                     System.out.print("Production Company: ");
                                     String prodCo = input.nextLine();
-                                    a.addMovieToDatabase(title, genre, descr, release, digital, lateFeeRate, lateFeeRate, oldPeriod, newPeriod, lateFeeRate, price, ageRate, directorLast, directorFirst, sequelTo, prodCo);
+                                    a.addMovieToDatabase(title, genre, descr, release, digital, oldRate, newRate, oldPeriod, newPeriod, lateFeeRate, price, ageRate, directorLast, directorFirst, sequelTo, prodCo);
+                                    i = 0;
+                                    break;
                                 }
                                 case "delete":{
                                     System.out.print("What is the title of the movie you want to delete? ");
                                     String title = input.nextLine();
                                     a.deleteMovieFromDatabase(title);
+                                    i = 0;
+                                    break;
                                 }
                                 case "manage inventory":{
                                     System.out.print("Would you like to increase or decrease the inventory for a movie?(enter 'increase' or 'decrease') ");
@@ -115,19 +130,30 @@ public class UNFMovieStore {
                                     switch (input.next()){
                                         case "increase":{
                                             System.out.print("What is the title of the movie you want to increase the inventory for? ");
+                                            input = new Scanner (System.in);
                                             String title = input.nextLine();
                                             a.increaseInventory(title);
+                                            i = 0;
+                                            break;
                                         }
                                         case "decrease":{
                                             System.out.print("What is the title of the movie you want to decrease the inventory for? ");
+                                            input = new Scanner (System.in);
                                             String title = input.nextLine();
                                             System.out.print("What is the SKU of the movie you want to decrease the inventory for? ");
+                                            input = new Scanner (System.in);
                                             int sku = input.nextInt();
                                             a.decreaseInventory(title, sku);
+                                            i = 0;
+                                            break;
                                         }
                                     }
+                                    i = 0;
+                                    break;
                                 }
                             }
+                            i = 0;
+                            break;
                         }
                         case "manage customer balance":{
                         //call checkUserBalance
@@ -138,6 +164,8 @@ public class UNFMovieStore {
                             switch (input.nextLine()){
                                 case "user balance":{
                                     a.checkUserBalance();
+                                    i = 0;
+                                    break;
                                 }
                                 case "rental rate":{
                                     System.out.print("What is old rate? ");
@@ -145,6 +173,8 @@ public class UNFMovieStore {
                                     System.out.print("What would you like to be new rate? ");
                                         double newRate = input.nextDouble();
                                     a.updateRentalRate(oldRate, newRate);
+                                    i = 0;
+                                    break;
                                 }
                                 case "late fee":{
                                     System.out.print("What would you like the new fee amount to be? ");
@@ -154,8 +184,12 @@ public class UNFMovieStore {
                                     System.out.print("What would you like the maximum rental period to be?(in days) ");
                                         int max = input.nextInt();
                                     a.updateLateFee(fee, max, max);
+                                    i = 0;
+                                    break;
                                 }
                             }
+                            i = 0;
+                            break;
                         }
                         case "generate reports":{
                         //call RevenueByTitleAndGenre and use getGenreIDFromGenre??
@@ -165,14 +199,22 @@ public class UNFMovieStore {
                             switch (input.nextLine()){
                                 case "by title":{
                                     a.revenueByTitleAndGenre();
+                                    i = 0;
+                                    break;
                                 }
                                 case "by period":{
                                     a.revenuePeriodic();
+                                    i = 0;
+                                    break;
                                 }
                             }
+                            i = 0;
+                            break;
                         }
+                    
                     }
-                    break;
+                }
+                break;
                 }
             case "customer":
                 {
@@ -183,10 +225,11 @@ public class UNFMovieStore {
                     System.out.print("Enter your password: ");
                     String password = input.next();
                     customer c;
-                    c = new customer();
-                    c.customer(lname, fname, password);
+                    c = new customer(lname, fname, password);
                     
-                    System.out.print("Hello "+fname+", How would you like to proceed? Please enter 'search movies', 'checkout', 'return', 'manage balance',or 'write review': ");
+                    int i = 0;
+                    while(i < 1){
+                    System.out.print(fname+", please select an action. Enter enter 'search movies', 'checkout', 'return', 'manage balance',or 'write review': ");
                     input = new Scanner (System.in);
                     switch (input.nextLine()){
                         case "search movies":{
@@ -194,21 +237,25 @@ public class UNFMovieStore {
                             System.out.print("Enter the title you would like to search for: ");
                             String search = input.nextLine();
                             c.searchMovieDatabase(search);
+                            i = 0;
+                            break;
                         }
                         case "checkout":{
                         //call addMovieToCart
                         //call checkout
-                            int i = 0;
-                            while(i < 1){
+                            int x = 0;
+                            while(x < 1){
                                 System.out.print("Would you like to 'rent' or 'buy': ");
                                 int type = -1;
                                 input = new Scanner (System.in);
                                 switch (input.next()){
                                     case "rent":{
                                         type = 1;
+                                        break;
                                     }
                                     case "buy":{
                                        type = 0;
+                                       break;
                                     }
                                 }   
                             
@@ -217,18 +264,22 @@ public class UNFMovieStore {
                                 int id = c.getMovieIDFromTitle(title);
                                 c.addMovieToCart(id, type);
                             
-                                System.out.print("Are you ready to check out? (enter 'yes' or 'no' ");
+                                System.out.print("Are you ready to check out? (enter 'yes' or 'no') ");
                                 input = new Scanner (System.in);
                                 switch (input.next()){
                                     case "yes":{
-                                        i = 1;
+                                        x = 1;
                                         c.checkout();
+                                        break;
                                     }
                                     case "no":{
-                                        i = 0;
+                                        x = 0;
+                                        break;
                                     }
                                 }
                             }
+                            i = 0;
+                            break;
                         }
                         case "return":{
                         //call makeReturn
@@ -236,20 +287,26 @@ public class UNFMovieStore {
                             String title = input.nextLine();
                             int id = c.getMovieIDFromTitle(title);
                             c.makeReturn(id);
+                            i = 0;
+                            break;
                         }
                         case "manage balance":{
                         //call checkBalance
                         //call payBalance
-                        System.out.print("Would you like to check or pay your balance? (enter 'check' or 'pay' ");
+                        System.out.print("Would you like to check or pay your balance? (enter 'check' or 'pay') ");
                         input = new Scanner (System.in);
                         switch (input.next()){
                                 case "check":{
                                     c.checkBalance();
+                                    break;
                                 }
                                 case "pay":{
                                     c.payBalance();
+                                    break;
                                 }
-                            }   
+                            }
+                            i = 0;
+                            break;
                         }
                         case "write review":{
                         //call writeReview
@@ -260,9 +317,12 @@ public class UNFMovieStore {
                         System.out.print("Enter the score of the movie: ");
                         int score = input.nextInt();
                         c.writeReview(text, score, title);
+                        i = 0;
+                        break;
                         }
                     }
-                    break;
+                }   
+                break;
                 }
             default:
                 System.out.println("Please try again and enter either 'employee' or 'customer'");
