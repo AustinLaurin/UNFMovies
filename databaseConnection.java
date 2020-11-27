8838b13d39a1e9bfb55cd49a16b70eeffa81d47d
AustinLaurin <63205477+AustinLaurin@users.noreply.github.com>
2020-11-27 16:52:56 -0500

    Update databaseConnection.java

--- databaseConnection.java
+++ databaseConnection.java
@@ -8,7 +8,7 @@ public class databaseConnection {
         try{
         //Make sure you login using the appropriate database url, account name, and account password for the corresponding data. 
         //The connection is supplied by this method to create ease of changing databases. Without, you would have to change it in every place a connection is made.
-         return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/unfmovies", "root", "password");
+         return DriverManager.getConnection("jdbc:mysql://cisvm-winsrv-mysql1.unfcsd.unf.edu:3308/group3", "n01040501", "Fall20200501");
         }
         catch(Exception e) {
             System.out.println(e.getMessage());
